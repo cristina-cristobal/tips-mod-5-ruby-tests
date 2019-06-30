232c9e7905fce527168709cacdbe3adecbe6cecb
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-06-30 18:24:34 -0400

    timeboxed out

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -8,23 +8,26 @@ class RecurringMoment
     @period = period
   end
 
+
   def match(date)
+
     current = @start
 
-    while current < date
-      if current == date
-        return true
-      end
-
-      if @period == 'monthly'
-        current = current.advance(weeks: @interval)
-      elsif @period = 'weekly'
-        current = current.advance(months: @interval)
-      elsif @period == 'daily'
-        current = current.advance(days: @interval)
-      end
+    # advance is a built in ruby method that uses Date to provide precise Time calculations. The options parameter takes a hash with keys years, months, weeks, days, hours, minutes.
+    
+    if @period == 'monthly'
+      current = current.advance(weeks: @interval)
+    elsif @period == 'weekly'
+      current = current.advance(months: @interval)
+    elsif @period == 'daily'
+      current = current.advance(days: @interval)
     end
 
-    return false
+    if current < date || current == date
+      return true
+    else return false
+    end
   end
+
+
 end
