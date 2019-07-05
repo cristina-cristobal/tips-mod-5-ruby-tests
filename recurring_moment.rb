027ab81cce22c130243872d48cfd6ba1e57de709
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-07-05 16:50:06 -0400

    passes all but Feb monthly ocurrences

--- recurring_moment.rb
+++ recurring_moment.rb
@@ -1,5 +1,7 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'byebug'
+require 'pry'
 
 class RecurringMoment
   def initialize(start:, interval:, period:)
@@ -12,22 +14,51 @@ class RecurringMoment
   def match(date)
 
     current = @start
-
+    array = []
     # advance is a built in ruby method that uses Date to provide precise Time calculations. The options parameter takes a hash with keys years, months, weeks, days, hours, minutes.
-    
-    if @period == 'monthly'
-      current = current.advance(weeks: @interval)
-    elsif @period == 'weekly'
-      current = current.advance(months: @interval)
-    elsif @period == 'daily'
-      current = current.advance(days: @interval)
-    end
 
-    if current < date || current == date
-      return true
-    else return false
+    if @period == 'monthly'
+      # while current < date, then increase by current.advance(sdfjew) until date is hit
+      until current > date do
+        current = current.advance(months: @interval)
+        if date == current
+          return true
+        end
+        # binding.pry
+      end
+      return false
+      # breaks at feb, instead, use @start and set counter ++
+    elsif @period == 'weekly'
+      until current > date do
+        current = current.advance(weeks: @interval)
+        if date == current
+          return true
+        end
+      end
+        return false
+    elsif @period == 'daily'
+      until current > date do
+      current = current.advance(days: @interval)
+        if date == current
+          return true
+        end
+      end
+        return false
     end
   end
 
+# loop through instances of the RecurringMoment
+# if loop contains date, then return true
+# if an instance of RecurringMoment > date, then break
+
+
+  #
+  #
+  #   if current == date
+  #     # current < date ||
+  #     return true
+  #   else return false
+  #   end
+  # end
 
 end
